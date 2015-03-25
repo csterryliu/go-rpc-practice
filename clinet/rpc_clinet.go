6d0d0ab8f5d6863d378df8fe65bd9e5ee1d51922
Terry Liu <terry_liu@xpec.com>
2015-03-25 15:36:24 +0800

    Complete very simple client and server

--- clinet/rpc_clinet.go
+++ clinet/rpc_clinet.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+        "fmt"
+        "net/rpc"
+)
+
+type Args struct {
+        A int
+        B int
+}
+
+func main() {
+        args := Args{5, 2}
+        var reply int
+        
+        client, err := rpc.Dial("tcp", "localhost:3000")  // connet to a rpc server
+        checkError(err)
+        
+        err = client.Call("Arith.Multiply", args, &reply)
+        checkError(err)
+        fmt.Printf("%d * %d = %d\n", args.A, args.B, reply)
+        
+        err = client.Call("Arith.Add", args, &reply)
+        checkError(err)
+        fmt.Printf("%d + %d = %d\n", args.A, args.B, reply)
+        
+}
+
+
+func checkError(err error) {
+        if err != nil {
+             fmt.Println(err)
+             return
+        }
+}
---
+++ server/rpc_server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+        "net"
+        "net/rpc"
+        "fmt"
+)
+
+type Arith int   // you can consider it as a service of RPC
+type Args struct {
+        A int
+        B int
+}
+func (a *Arith) Add(args *Args, reply *int) error {  // a method of service
+        *reply = args.A + args.B
+        return nil
+}
+func (a *Arith) Multiply(args *Args, reply *int) error {  // a method of service
+        *reply = args.A * args.B
+        return nil
+}
+
+
+func main() {
+        service := new(Arith)  // this service contains 2 methods
+        rpc.Register(service)  // register this service to DefaultServer. the name of this service will be its concrete type
+        
+        tcpAddr, err := net.ResolveTCPAddr("tcp", ":3000")
+        checkError(err)
+        listener, err := net.ListenTCP("tcp", tcpAddr)
+        checkError(err)
+        
+        // Let DefaultServer starts to wait for requests
+        fmt.Println("start running...")
+        rpc.Accept(listener)
+        
+}
+
+
+func checkError(err error) {
+        if err != nil {
+             fmt.Println(err)
+             return
+        }
+}
